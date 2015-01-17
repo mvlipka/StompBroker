3166222dcfce13fb2c4ff1c8c9e9b5ed8067be18
unknown-user <unknown-user@fe80::28c8:3b21:4335:7ab8%4>
2015-01-17 14:56:19 -0600

    Initial check-in of module StompBroker

---
+++ StompBroker/Client.d
@@ -0,0 +1,12 @@
+﻿module Client;
+import std.socket;
+import std.stdio;
+synchronized class Client
+{
+	Socket * socket;
+	this(shared Socket * sock) shared
+	{
+		this.socket = sock;
+	}
+}
+
---
+++ StompBroker/main.d
@@ -0,0 +1,28 @@
+﻿module main;
+
+import std.stdio;
+import std.socket;
+import std.concurrency;
+
+import Client;
+
+void handleClient(shared Client * client){
+	char[1024] buf;
+	auto message = client.receive(buf);
+}
+
+void main(string[] args)
+{
+	writeln("Starting Server...");
+	TcpSocket serverSocket = new TcpSocket(AddressFamily.INET);
+	serverSocket.bind(new InternetAddress(1999));
+	serverSocket.listen(5);
+	writeln("Server is listning on port: ", serverSocket.localAddress.toPortString());
+	while(true){
+		Socket clientSocket = serverSocket.accept();
+		writeln(clientSocket.remoteAddress);
+		shared Client client = new shared Client(cast(shared)clientSocket);
+		spawn(&handleClient, client);
+	}
+}
+
