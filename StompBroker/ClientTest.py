0d55426d85f2027363b9afc1a2ba119c0ebfc12f
mvlipka <mvlipka@gmail.com>
2015-01-18 22:46:45 -0600

    Added a Python client for testing the server

---
+++ StompBroker/ClientTest.py
@@ -0,0 +1,42 @@
+﻿import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 1999))
+
+
+message = "CONNECT\n"
+message += "login: mvlipka\n"
+message += "passcode: lipka\n"
+message += "^@"
+s.send(bytes(message, 'UTF-8'));
+message = s.recv(1024)
+print(message)
+
+
+message = "SUBSCRIBE\n"
+message += "destination: /mychannel\n"
+message += "ack:client\n"
+message += "^@"
+s.send(bytes(message, 'UTF-8'))
+
+
+message = "SEND\n"
+message += "destination: /mychannel\n"
+message += "HEY EVERYONE\n"
+message += "^@"
+s.send(bytes(message, 'UTF-8'))
+message = s.recv(1024)
+print(message)
+
+
+message = "UNSUBSCRIBE\n"
+message += "destination: /mychannel\n"
+message += "^@"
+s.send(bytes(message, 'UTF-8'))
+
+
+message = "DISCONNECT\n"
+message += "^@"
+s.send(bytes(message, 'UTF-8'))
+
+s.close()
